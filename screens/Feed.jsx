6026b0b41652422b1d8a5b2012a6adec78a53e71
hoodiedan <obodedaniel3@gmail.com>
2024-06-13 05:19:33 +0100

    initial commit: video scroll works

---
+++ screens/Feed.jsx
@@ -0,0 +1,36 @@
+import { View, FlatList, StyleSheet } from 'react-native';
+import React, { useRef, useState } from 'react';
+import VideoItem from '../components/VideoItem';
+import { videos } from '../data/videos';
+
+export default function FeedScreen() {
+  const [currentViewableItemIndex, setCurrentViewableItemIndex] = useState(0);
+  const viewabilityConfig = { viewAreaCoveragePercentThreshold: 50 }
+  const onViewableItemsChanged = ({ viewableItems }) => {
+    if (viewableItems.length > 0) {
+      setCurrentViewableItemIndex(viewableItems[0].index ?? 0);
+    }
+  }
+  const viewabilityConfigCallbackPairs = useRef([{ viewabilityConfig, onViewableItemsChanged }])
+  return (
+    <View style={styles.container}>
+      <FlatList
+            data={videos}
+            renderItem={({ item, index }) => (
+                <VideoItem item={item} shouldPlay={index === currentViewableItemIndex} />
+            )}
+            keyExtractor={item => item}
+            pagingEnabled
+            horizontal={false}
+            showsVerticalScrollIndicator={false}
+            viewabilityConfigCallbackPairs={viewabilityConfigCallbackPairs.current}
+        />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+});
